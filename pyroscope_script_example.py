e11f6bc1512c926425bf73ee48c44ccc3f27c802
David Banks <amoebae@gmail.com>
2016-02-14 16:44:14 +0000

    null out things

--- pyroscope_script_example.py
+++ pyroscope_script_example.py
@@ -23,10 +23,8 @@ import pdb
 
 
 
-class UserScript(base.ScriptBaseWithConfig):
-    """
-        Just some script you wrote.
-    """
+class RtorrentLowSpaceDriver(base.ScriptBaseWithConfig):
+    """rtorrent low space driver"""
 
     # argument description for the usage information
     ARGS_HELP = "<arg_1>... <arg_n>"
@@ -34,89 +32,17 @@ class UserScript(base.ScriptBaseWithConfig):
     # five gigabyte space limit
     SPACE_LIMIT = 5 * (2 ** 30);
 
-
     def add_options(self):
-        """ Add program options.
-        """
-        super(UserScript, self).add_options()
-
+        super(RtorrentLowSpaceDriver, self).add_options()
         # basic options
         ##self.add_bool_option("-n", "--dry-run",
         ##    help="don't do anything, just tell what would happen")
 
 
     def mainloop(self):
-        """ The main loop.
-        """
-        # Grab your magic wand
         proxy = config.engine.open()
-
         # store hash in external file
         infohash = open('hash.txt').read().rstrip()
-
-
-        # Wave it
-        torrents = list(config.engine.items())
-
-        call_idx = getattr(config.engine._rpc.d, 'get_size_files')
-        file_len = call_idx(infohash)
-
-        call = getattr(config.engine._rpc.f, 'get_size_bytes')
-#        print repr(call)
-
-
-        set_prio = getattr(config.engine._rpc.f, 'set_priority')
-        get_completed_chunks = getattr(config.engine._rpc.f, 'get_completed_chunks')
-        get_size_chunks = getattr(config.engine._rpc.f, 'get_size_chunks')
-        get_path = getattr(config.engine._rpc.f, 'get_path')
-        
-
-#        print call(infohash + ":f1")
-
-        file_list = []
-
-        for i in range(file_len):
-            id_ = infohash + ":f" + str(i)
-            size = call(id_)
-            set_prio(id_, 0)
-            path = get_path(id_)
-            cmp_chunks = get_completed_chunks(id_)
-            size_chunks = get_size_chunks(id_)
-#            pprint(cmp_chunks)
-#            pprint(size_chunks)
-
-            percentage = int((float(cmp_chunks) / float(size_chunks)) * 100)
-
-
-            print "%s: %d%%" % (path, percentage)
-            
-            file_info = {
-                "id": id_,
-                "size": size,
-            }
-
-            file_list.append(file_info)
-
-        file_list.sort(key=lambda x: x['size'])
-#        pprint(file_list)
-
-        group = []
-        size_so_far = 0
-
-        for file_ in file_list:
-            this_size = file_['size']
-            if (size_so_far + this_size) > self.SPACE_LIMIT:
-                break
-            size_so_far += this_size
-            group.append(file_)
-
-
-        for file_ in group:
-            set_prio(file_['id'], 1)
-
-
-        call2 = getattr(config.engine._rpc.d, 'update_priorities')
-        call2(infohash)
 
         self.LOG.info("XMLRPC stats: %s" % proxy)
 
@@ -124,6 +50,6 @@ class UserScript(base.ScriptBaseWithConfig):
 
 if __name__ == "__main__":
     base.ScriptBase.setup()
-    UserScript().run()
+    RtorrentLowSpaceDriver().run()
 
 
