6cbcd51a3cb661d32e7a3a2628bba2220ee1f5ad
David Banks <amoebae@gmail.com>
2016-02-18 18:05:17 +0000

    read torrent dir and find size

--- main.py
+++ main.py
@@ -4,13 +4,34 @@ import sys
 import logging
 from logging import debug, info
 import argparse
+import libtorrent
+import os
+
+# Algorithm for this.
+
+# Can it run from cron.
+# Needs to find the size of each torrent in the torrent dir,
+# which must be separate from rtorrent's watched dir.
+# Sort the list by size, smallest first.
+# Now add the torrents to the watched dir.
+# We guarantee that -- EITHER multiple torrents are present.  If multiple torrents are present, then the total can fit inside the space limit.
+# OR a single torrent is present, in which case it may not fit inside the space limit.
+# Can the total fit on the disk?
 
 class RtorrentLowSpaceDriver(object):
+    MANAGED_TORRENTS_DIRECTORY = "/home/amoe/download/torrents"
+
     def run(self, args):
         ns = self.initialize(args)
         
         info("Starting.")
-        print ns.rest_args
+
+        for torrent in os.listdir(self.MANAGED_TORRENTS_DIRECTORY):
+            full_path = os.path.join(self.MANAGED_TORRENTS_DIRECTORY, torrent)
+            t_info = libtorrent.torrent_info(full_path)
+            print t_info.info_hash(), ":", t_info.total_size()
+
+
         info("End.")
         
     def initialize(self, args):
