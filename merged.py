27b3e921d01b618a1f28cf9ef25bc5aef6d1ad4b
David Banks <amoebae@gmail.com>
2016-02-20 14:53:23 +0000

    small strat seems to work

--- merged.py
+++ merged.py
@@ -7,6 +7,8 @@ import argparse
 import os
 import rtorrent_xmlrpc
 import pprint
+import libtorrent
+import subprocess
 
 def splitter(data, pred):
     yes, no = [], []
@@ -19,10 +21,10 @@ def splitter(data, pred):
 
 class RtorrentLowSpaceDriver(object):
     MANAGED_TORRENTS_DIRECTORY = "/home/amoe/dev/rtorrent_low_space_driver/managed"
-    REMOTE_HOST = "kupukupu"
-    REMOTE_PATH = "/mnt/spock/mirror2"
+    REMOTE_HOST = "localhost"
+    REMOTE_PATH = "/home/amoe/mymirror"
     SPACE_LIMIT = 3 * (2 ** 30)
-    REQUIRED_RATIO = 1.0
+    REQUIRED_RATIO = 0.0
 
     server = None
     
@@ -35,11 +37,56 @@ class RtorrentLowSpaceDriver(object):
             "scgi:///home/amoe/.rtorrent.sock"
         )
 
-        self.handle_small_torrents_strategy()
-        self.handle_large_torrent_strategy()
+
+        large_torrent = self.check_for_large_managed_torrents()
+
+        if large_torrent is not None:
+            info("Detected incomplete & already loaded large torrent.  Switching to large strategy.")
+            info("Torrent is %s" + pprint.pformat(large_torrent))
+            self.handle_large_torrent_strategy(large_torrent_infohash)
+        else:
+            info("Using small torrents strategy per default.")
+            load_candidates, load_choices = self.handle_small_torrents_strategy()
+            info("Small torrents strategy finished.")
+            if not load_choices:
+                if load_candidates:
+                    info("Large torrents blocked by small strategy.  Switching to large strategy.")
+                    large_torrent = sorted(load_candidates, lambda t: t['size'])
+                    self.load_torrents(large_torrent[0])
+                    self.handle_large_torrent_strategy()
+                else:
+                    info("No candidates to load.  Either all torrents are already loaded, or there are no torrents in the managed directory.")
+                    info("For you to verify, said managed torrent list is %s" % pprint.pformat(self.build_managed_torrents_list()))
+                    info("Now quietly exiting successfully.")
+            else:
+                info("Small strategy succeeded.  See you next time!")
+
         info("Done.")
 
     ## SMALL TORRENT STRATEGY
+
+    def check_for_large_managed_torrents(self):
+        # XXX: we build this list twice
+        managed_torrents = self.build_managed_torrents_list()
+        rt_complete, rt_incomplete = self.get_torrents_from_rtorrent()
+        
+        # XXX: is rt_incomplete correct here?
+        managed_torrents_in_client = [
+            managed_torrents[t] for t in rt_incomplete
+        ]
+        
+        large_managed_torrents = [
+            t for t in managed_torrents_in_client
+            if t['size'] > self.SPACE_LIMIT
+        ]
+
+        if large_managed_torrents:
+            if len(managed_torrents_in_client) != 1:
+                raise Exception("weird condition, there should only be one large torrent at once")
+            
+            return large_managed_torrents[0]
+        else:
+            return None
 
     def handle_small_torrents_strategy(self):
         managed_torrents = self.build_managed_torrents_list()
@@ -63,6 +110,8 @@ class RtorrentLowSpaceDriver(object):
 
         info("Decided to load these torrents: %s" % pprint.pformat(load_choices))
         self.load_torrents(load_choices)
+
+        return load_candidates, load_choices
 
     # make lookup table for torrents, should be a set
     def build_managed_torrents_list(self):
@@ -89,17 +138,17 @@ class RtorrentLowSpaceDriver(object):
 
     def sync_and_remove(self, torrent_list):
         for completed_torrent in torrent_list:
-            infohash_ = completed_torrent['hash']
+            infohash = completed_torrent['hash']
             info("Handling completed torrent: %s" % completed_torrent['name'])
 
-            ratio = server.d.get_ratio(infohash)
+            ratio = self.server.d.get_ratio(infohash)
             if ratio < self.REQUIRED_RATIO:
                 info("Torrent is completed but not seeded to required ratio.  Skipping.")
                 continue
 
-            base_path = server.d.get_base_path(infohash)
+            base_path = self.server.d.get_base_path(infohash)
             self.sync_completed_path_to_remote(base_path)
-            server.d.erase(infohash)
+            self.server.d.erase(infohash)
 
             if os.path.isdir(base_path):
                 shutil.rmtree(base_path)
@@ -117,7 +166,7 @@ class RtorrentLowSpaceDriver(object):
         # Count incomplete torrents
         cumulative_incomplete_size = 0
         for incomplete_torrent in torrent_list:
-            cumulative_incomplete_size += managed_torrent['size']
+            cumulative_incomplete_size += incomplete_torrent['size']
 
         info("Cumulative size of incomplete items was %d" % cumulative_incomplete_size)
         effective_available_size = self.SPACE_LIMIT - cumulative_incomplete_size
@@ -130,7 +179,7 @@ class RtorrentLowSpaceDriver(object):
         # Filter out the managed items that were already loaded
         not_already_loaded = []
         for k, v in managed_group.iteritems():
-            if k not in rt_incomplete and k not in rt_complete:
+            if k not in incomplete_group and k not in complete_group:
                 not_already_loaded.append(v)
 
         return not_already_loaded
@@ -151,7 +200,24 @@ class RtorrentLowSpaceDriver(object):
 
     def load_torrents(self, torrent_paths):
         for torrent_to_load in torrent_paths:
-            server.load_start(torrent_to_load['torrent_path'])
+            self.server.load_start(torrent_to_load['torrent_path'])
+
+
+    def sync_completed_path_to_remote(self, source_path):
+        cmd = [
+            "rsync", "-aPv", source_path, 
+            self.REMOTE_HOST + ":" + self.REMOTE_PATH
+        ]
+
+        while True:
+            try:
+                info("running command: %s", ' '.join(cmd))
+                subprocess.check_call(cmd)
+                return
+            except subprocess.CalledProcessError, e:
+                error("failed to sync files to remote, retrying.  exception was '%s'" % e)
+                time.sleep(60)
+
 
 
     ## LARGE TORRENT STRATEGY
